1dc52318e47a7ad4de647311c2a1a7da4a30acf3
Alperen Sarıçayır <alperensarcayr@gmail.com>
2025-05-04 15:11:54 +0300

    Add files via upload

---
+++ custom-attribute-odevi-alperen-saricayir/custom-attribute-odevi-alperen-saricayir/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace custom_attribute_odevi_alperen_saricayir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtBolum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(532, 118);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(184, 22);
+            this.txtAd.TabIndex = 0;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(532, 146);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(184, 22);
+            this.txtSoyad.TabIndex = 1;
+            // 
+            // txtBolum
+            // 
+            this.txtBolum.Location = new System.Drawing.Point(532, 174);
+            this.txtBolum.Name = "txtBolum";
+            this.txtBolum.Size = new System.Drawing.Size(184, 22);
+            this.txtBolum.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(497, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ADI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(469, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "SOYADI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(462, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "BÖLÜMÜ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(591, 212);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(55, 16);
+            this.lblSonuc.TabIndex = 6;
+            this.lblSonuc.Text = "SONUC";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Location = new System.Drawing.Point(722, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "DOGRULA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(1164, 510);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBolum);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtBolum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ custom-attribute-odevi-alperen-saricayir/custom-attribute-odevi-alperen-saricayir/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace custom_attribute_odevi_alperen_saricayir
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSonuc.Text = "";
+            txtAd.BackColor = txtSoyad.BackColor = txtBolum.BackColor = System.Drawing.Color.White;
+        }
+
+        [AttributeUsage(AttributeTargets.Property)]
+        public class ZorunluAlanAttribute : Attribute
+        {
+            public string HataMesaji { get; set; }
+
+            public ZorunluAlanAttribute(string hataMesaji)
+            {
+                HataMesaji = hataMesaji;
+            }
+        }
+
+        public class Ogrenci
+        {
+            [ZorunluAlan("Öğrenci Adı boş bırakılamaz!")]
+            public string Ad { get; set; }
+
+            [ZorunluAlan("Öğrenci Soyadı boş bırakılamaz!")]
+            public string Soyad { get; set; }
+
+            [ZorunluAlan("Öğrenci Bölümü boş bırakılamaz!")]
+            public string Bolum { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci ogr = new Ogrenci
+            {
+                Ad = txtAd.Text.Trim(),
+                Soyad = txtSoyad.Text.Trim(),
+                Bolum = txtBolum.Text.Trim()
+            };
+
+            bool hataVarMi = false;
+            string hataMesajlari = "";
+
+            PropertyInfo[] ozellikler = typeof(Ogrenci).GetProperties();
+
+            foreach (var ozellik in ozellikler)
+            {
+                var deger = ozellik.GetValue(ogr) as string;
+
+                var attribute = (ZorunluAlanAttribute)ozellik
+                    .GetCustomAttributes(typeof(ZorunluAlanAttribute), false)
+                    .FirstOrDefault();
+
+                if (attribute != null && string.IsNullOrWhiteSpace(deger))
+                {
+                    hataMesajlari += $"{attribute.HataMesaji}\n";
+                    hataVarMi = true;
+
+                    // Boş olan alanları renklendir
+                    if (ozellik.Name == "Ad") txtAd.BackColor = System.Drawing.Color.MistyRose;
+                    if (ozellik.Name == "Soyad") txtSoyad.BackColor = System.Drawing.Color.MistyRose;
+                    if (ozellik.Name == "Bolum") txtBolum.BackColor = System.Drawing.Color.MistyRose;
+                }
+            }
+
+            if (hataVarMi)
+            {
+                MessageBox.Show(hataMesajlari, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                lblSonuc.Text = $"Ad: {ogr.Ad}\nSoyad: {ogr.Soyad}\nBölüm: {ogr.Bolum}";
+                txtAd.BackColor = txtSoyad.BackColor = txtBolum.BackColor = System.Drawing.Color.White;
+            }
+        }
+    }
+}
